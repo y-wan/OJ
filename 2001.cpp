250be9a0b6242c1be4a7c98a41f70d3549d66008
Yingge WAN <y-wan@users.noreply.github.com>
2017-08-11 14:55:04 +0800

    Create 2001.cpp

---
+++ 2001.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, a0, b0, tmp1, tmp2;
+	cin >> a >> b >> tmp1 >> b0 >> a0 >> tmp2;
+	cout << a-a0 << ' ' << b-b0 << endl;
+	return 0;
+}
