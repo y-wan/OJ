501b899d07fa430651ba42f37f31ac28de79eaf5
Yingge WAN <y-wan@users.noreply.github.com>
2017-08-14 11:07:38 +0800

    Create 1203.cpp

---
+++ 1203.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+pair<int, int> a[100000];
+
+int main() {
+	//ios_base::sync_with_stdio(false);
+	int n, end = -1, p = 0, sum = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> a[i].second >> a[i].first;
+	sort(a, a + n);
+	for (int i = a[0].first; i <= a[n - 1].first; i++) {
+		for (; a[p].first <= i; p++) {
+			if (a[p].second > end) {
+				end = a[p].first;
+				sum++;
+			}
+		}
+	}
+	cout << sum << endl;
+	return 0;
+}
